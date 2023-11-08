fa398627796e184ab02d44c76fb06610f5dee7f3
RogelioTeranSuarez <20151602@aguascalientes.tecnm.mx>
2023-11-07 19:43:52 -0600

    Objetos desde JSON

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <nav className="bg-White mt-10">
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="flex items-center justify-between h-16">
-        <img src="../src/assets/logo.png" class="h-20">
+        <img src="./assets/logo.png" class="h-20">
         <div className="flex items-baseline space-x-4">
           <router-link to="/" 
           class="text-black hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Inicio</router-link>
