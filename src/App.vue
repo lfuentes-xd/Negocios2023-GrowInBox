96883628d9c5b624c543ef2232e2670484b66f69
Carlos Mauricio Macías González <carlitos.mauricio08@hotmail.com>
2023-11-16 20:25:08 -0600

    links m

--- src/App.vue
+++ src/App.vue
@@ -6,28 +6,28 @@
         <img src="./assets/logo.png" class="h-20" alt="imagen del logo" title="Grow in box logo">
         <div className="flex items-baseline space-x-4">
           <router-link to="/" 
-          class="text-black hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Inicio</router-link>
+          class="text-black hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" title="home">Inicio</router-link>
           <router-link to="/Catalogo"
-            className="text-black hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Cátalogo</router-link>
+            className="text-black hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" title="catalogo">Cátalogo</router-link>
           <router-link to=""
-            className="text-black hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contanto</router-link>
+            className="text-black hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" title="contacto">Contanto</router-link>
         </div>
 
         <div class="flex space-x-4"> 
 
-          <router-link to="login" className="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" title="login">
+          <router-link to="login" className="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
             <img class="w-6 h-6" src="../src/assets/Search.png" alt="search logo" title="logo search">
           </router-link>
           
-          <router-link to="login" className="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" title="login">
+          <router-link to="login" className="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
             <img class="w-6 h-6" src="../src/assets/User.png" alt="User logo" title="logo user">
           </router-link>
           
-          <router-link to="login" className="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" title="login">
+          <router-link to="login" className="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
             <img class="w-6 h-6" src="../src/assets/Heart.png" alt="heart logo" title="logo heart">
           </router-link>
           
-          <router-link to="login" className="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" title="login">
+          <router-link to="login" className="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
             <img class="w-6 h-6" src="../src/assets/shopbag.png" alt="bagshop logo" title="logo shopbag">
           </router-link>
           
