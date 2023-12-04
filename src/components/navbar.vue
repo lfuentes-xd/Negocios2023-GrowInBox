f188ad48dfd902395c5f323210f8af8e44c6351d
victor <20151713@aguascalientes.tecnm.mx>
2023-12-03 21:00:36 -0600

    login menu

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -21,8 +21,13 @@
                         title="Usuario">
                         <img class="w-6 h-6" src="../assets/User.png" alt="User logo" title="logo user">
                     </router-link>
-
                     
+                    <router-link to="/OrdenesCompra"
+                        className="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                        title="Usuario">
+                        Órdenes de compra
+                    </router-link>
+
                     <router-link to="/OrdenesCompra"
                         className="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                         title="Usuario">
