758b295b141e8cb2c13a396770566b85a8803589
TacoBestialIntergalactico <notropet@gmail.com>
2023-12-04 09:28:15 -0600

    Correcion de Errores y Merges

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -45,12 +45,14 @@
             Forget Password?
           </a>
 
-          <button
+          <router-link to="/Productadd">
+          <button router-link to="/Productadd"
             type="submit"
             class="px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
           >
             Iniciar Sesión
           </button>
+        </router-link>
         </div>
       </form>
     </div>
--- src/components/OrdenesCompra.vue
+++ src/components/OrdenesCompra.vue
@@ -1,31 +1,35 @@
+este código es de OrdenesCompra:
+
 <template>
-    <navbar></navbar>
-    <div id="div">
+    <div>
+      <navbar></navbar>
+      <div class="container mx-auto p-4">
         <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-            Productos comprados por {{ usuarioP.nombre }}
+          Productos comprados por {{ usuarioP.nombre }}
         </h5>
         <br>
-
-        <table class="min-w-full">
+  
+        <div class="overflow-x-auto">
+          <table class="min-w-full table-auto">
             <thead class="bg-gray-200 border-b text-center">
-                <th>id</th>
-                <th>Nombre</th>
-                <th>Descripción</th>
-                <th>Precio</th>
+              <th class="w-1/4">ID</th>
+              <th class="w-1/4">Nombre</th>
+              <th class="w-1/4">Descripción</th>
+              <th class="w-1/4">Precio</th>
             </thead>
             <tbody>
-                <tr v-for="producto in usuarioP.productosuser" :key="producto.id" class="bg-white border-b ">
-                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ producto.id }}</td>
-                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ producto.nombre }}</td>
-                <td class="px-6 py-4 text-gray-900">{{ producto.descripcion }}</td>
-                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ producto.precio }}</td>
-                </tr>
+              <tr v-for="producto in usuarioP.productosuser" :key="producto.id" class="bg-white border-b">
+                <td class="px-4 py-2 whitespace-nowrap text-sm font-medium text-gray-900 max-w-xs">{{ producto.id }}</td>
+                <td class="px-4 py-2 whitespace-nowrap text-sm font-medium text-gray-900 max-w-xs">{{ producto.nombre }}</td>
+                <td class="px-4 py-2 whitespace-normal text-gray-900 max-w-xs">{{ producto.descripcion }}</td>
+                <td class="px-4 py-2 whitespace-nowrap text-sm font-medium text-gray-900 max-w-xs">{{ producto.precio }}</td>
+              </tr>
             </tbody>
-        </table>
-
-
+          </table>
+        </div>
+      </div>
     </div>
-</template>
+  </template>
 
 <script>
 import navbar from "../components/navbar.vue"
